a61f93dbf870c75ae776d8d555879078b3a3e437
jayantroy <141987105+reconroy@users.noreply.github.com>
2025-04-04 22:34:13 +0530

    added env

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 console.log("Hello World");
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
 });
